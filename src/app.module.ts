a55adf9dd9f333b40e7b5e1f157853de144dff4d
Javier González Martín <javier.gonzalez@omniaccess.com>
2026-05-04 11:06:53 +0200

    fix: lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { TenantModule } from './modules/tenant/tenant.module';
